ba04a9f4f62c8ddc3f9f91b68c8b1cef38069f62
hoangphuc1998 <trangtrunghoangphuc@gmail.com>
2020-03-08 12:53:05 +0700

    Front end basic usage

--- Client/urls.py
+++ Client/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import home, retrieve_images
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('query/', retrieve_images, name='query')
 ]
--- Client/views.py
+++ Client/views.py
@@ -12,5 +12,12 @@ def send_image_retrieval_request(caption, dist_func='cosine', k=10):
     return images, dists
 
 def home(request):
-    #images, dists = send_image_retrieval_request('toilet')
-    return render(request, 'client_home.html', {'images': range(10)})
+    return render(request, 'client_home.html', {'caption':''})
+
+def retrieve_images(request):
+    if request.method == 'GET':
+        caption = request.GET.get('caption')
+        images, _ = send_image_retrieval_request(caption)
+        return render(request, 'client_query.html', {'images': images, 'caption': caption})
+    else:
+        return render(request, 'client_home.html', {'caption':''})
