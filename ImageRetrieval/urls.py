ebb12feb937b55ea3f9cc8b756f5d9fba9170b14
hoangphuc1998 <trangtrunghoangphuc@gmail.com>
2020-03-12 00:53:25 +0700

    Split computation for large dataset

--- ImageRetrieval/urls.py
+++ ImageRetrieval/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('server/', include('Server.urls')),
-    path('client/', include('Client.urls')),
+    path('', include('Client.urls')),
     path('admin/', admin.site.urls),
 ]
