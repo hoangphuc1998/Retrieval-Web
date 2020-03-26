af1e58f31078c8ab739e0abf7fb84b2bc058b43b
lehoanganh298 <lehoanganh29896@gmail.com>
2020-03-26 21:15:01 +0700

    Change global paths

--- Server/views.py
+++ Server/views.py
@@ -9,20 +9,20 @@ import base64
 # Create your views here.
 
 # Global variables
-path = {    
+
+option_dict_path='/home/lehoanganh298/Projects/models/DualEncoding/options.pkl'
+text_encoder_path='/home/lehoanganh298/Projects/models/DualEncoding/DualEncoding_Ver2/best/text_encoder.pth'
+
+dataset_path = {    
     'COCO':{
-        'image_feature_folder' : '/home/lehoanganh298/Documents/COCO_val_image_transform',
-        'filename_folder' : '/home/lehoanganh298/Documents/COCO_filename',
-        'option_dict_path' : '/home/lehoanganh298/Documents/options.pkl',
-        'text_encoder_path' : '/home/lehoanganh298/Documents/DualEncoding_Ver2/best/text_encoder.pth',
-        'image_folder' : '/home/lehoanganh298/Documents/val2014',
+        'image_feature_folder' : '/home/lehoanganh298/Projects/datasets/COCO/COCO_val_image_transform',
+        'filename_folder' : '/home/lehoanganh298/Projects/datasets/COCO/COCO_filename',
+        'image_folder' : '/home/lehoanganh298/Projects/datasets/COCO/val2014',
     },
     'LSC': {
-        'image_feature_folder' : '/home/lehoanganh298/Documents/LSC_transform',
-        'filename_folder' : '/home/lehoanganh298/Documents/LSC_filename',
-        'option_dict_path' : '/home/lehoanganh298/Documents/options.pkl',
-        'text_encoder_path' : '/home/lehoanganh298/Documents/DualEncoding_Ver2/best/text_encoder.pth',
-        'image_folder' : '/home/lehoanganh298/Projects/Volumes/Samsung_T5/DATASETS/LSC2020',
+        'image_feature_folder' : '/home/lehoanganh298/Projects/datasets/LSC2020/LSC_transform',
+        'filename_folder' : '/home/lehoanganh298/Projects/datasets/LSC2020/LSC_filename',
+        'image_folder' : '/home/lehoanganh298/Projects/datasets/LSC2020/Volumes/Samsung_T5/DATASETS/LSC2020',
     }
 }
 text_model = None
@@ -37,11 +37,11 @@ def home(request):
     Load all model and features to memory
     '''
     global text_model, text_tokenizer, text_encoder, image_features, image_names
-    with open(path['COCO']['option_dict_path'], 'rb') as f:
+    with open(option_dict_path, 'rb') as f:
         opt = pickle.load(f)
     text_model, text_tokenizer = load_text_model(
         opt['text_model_type'], opt['text_model_pretrained'], device)
-    text_encoder = load_transform_model(opt, path['COCO']['text_encoder_path'], device)
+    text_encoder = load_transform_model(opt, text_encoder_path, device)
     # image_features, image_names = load_all_feature(
     #     image_feature_path, filename_path, device)
     return HttpResponse('Setup done!')
@@ -54,8 +54,8 @@ def get_images(request, caption, dataset, dist_func, k, start_from):
         dist_func = euclidean_dist
     dists, filenames = get_images_from_caption(caption=caption,
                                                 dataset=dataset,
-                                              image_features_folder=path[dataset]['image_feature_folder'],
-                                              image_names_folder=path[dataset]['filename_folder'],
+                                              image_features_folder=dataset_path[dataset]['image_feature_folder'],
+                                              image_names_folder=dataset_path[dataset]['filename_folder'],
                                               text_model=text_model,
                                               text_tokenizer=text_tokenizer,
                                               text_encoder=text_encoder,
@@ -66,7 +66,7 @@ def get_images(request, caption, dataset, dist_func, k, start_from):
     response_data['image'] = []
     #TODO: Add different image dataset
     for filename in filenames:
-        with open(os.path.join(path[dataset]['image_folder'], filename), 'rb') as f:
+        with open(os.path.join(dataset_path[dataset]['image_folder'], filename), 'rb') as f:
             response_data['image'].append(base64.b64encode(f.read()).decode('utf-8'))
     response_data['dists'] = dists.tolist()
     # print(dists)
